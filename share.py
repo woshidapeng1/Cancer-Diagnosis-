be8bc7c62ac88e626f3916a944c4e234982ec49d
woshidapeng1 <58641406+woshidapeng1@users.noreply.github.com>
2021-07-29 23:30:51 +0800

    Add files via upload

---
+++ share.py
@@ -0,0 +1,10 @@
+class SI:
+    mainWin = None
+    upload = None
+    select = None
+    start = None
+    back = None
+    archive = None
+    library = None
+    setting = None
+    speech = None
